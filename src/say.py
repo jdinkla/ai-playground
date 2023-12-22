c0589932cbe45a7a1415f04baef06e49cd756c08
Jörn Dinkla <jdinkla@thoughtworks.com>
2023-12-22 23:13:49 +0100

    add speech example

---
+++ src/say.py
@@ -0,0 +1,23 @@
+from utilities import init
+import argparse
+from pathlib import Path
+from openai import OpenAI
+from utilities import init, play_mp3
+
+init()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("message", help="the message to speak", default="Ernie and Bert eat cookies")
+args = parser.parse_args()
+
+client = OpenAI()
+
+speech_file_path = Path(__file__).parent / ".." / "speech.mp3"
+response = client.audio.speech.create(
+  model="tts-1",
+  voice="nova",
+  input=args.message
+)
+
+response.stream_to_file(speech_file_path)
+play_mp3(speech_file_path)
--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import pygame
 
 def init():
     _check_env()
@@ -10,3 +11,10 @@ def _check_env():
     if 'OPENAI_API_KEY' not in os.environ:
         print("OPENAI_API_KEY is not set. Exiting.")
         sys.exit(1)
+
+def play_mp3(path_to_mp3):
+    pygame.mixer.init()
+    pygame.mixer.music.load(path_to_mp3)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
