737975d328f01cda12f49b53c0341c35756ea898
Jörn Dinkla <jdinkla@thoughtworks.com>
2023-12-23 22:14:13 +0100

    add detective

---
+++ src/openai_detective.py
@@ -0,0 +1,45 @@
+"""
+Simple completion using OpenAI API
+"""
+
+import argparse
+import logging
+from openai import OpenAI
+from utilities import init
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="the name of a file with a story")
+args = parser.parse_args()
+
+init()
+
+
+PROMPT="""
+You are an investigator and a detective.
+The user will present you with a story of a crime and you will have to solve it. Explain your reasoning.
+"""
+
+with open(args.filename, 'r') as file:
+    content = file.read()
+
+#MODEL = "gpt-3.5-turbo-1106"
+#MODEL = "gpt-4-32k-0613"
+MODEL = "gpt-4-1106-preview"
+
+client = OpenAI()
+response = client.chat.completions.create(
+    model=MODEL,
+    messages=[
+        {
+            "role": "system",
+            "content": PROMPT
+        },
+        {
+            "role": "user",
+            "content": content
+        }
+    ]
+)
+
+logging.debug(response)
+print(response.choices[0].message.content)
