dd1e68edb6cc86cbb51a4ff3fba865d892678374
Jörn Dinkla <jdinkla@thoughtworks.com>
2023-12-24 14:35:29 +0100

    refactor dialogue

--- src/openai_dialogue.py
+++ src/openai_dialogue.py
@@ -20,8 +20,8 @@ Since 5 years the economy is in misarray because of the high inflation.
 The people are increasingly unhappy and looking for alternatives.
 Almost all citizens are watching the discussion, so it is very important to win the discussion.
 
-The A party believes similiar to the novel "Fahrenheit 451" that books are dangerous and should be banned. 
-The book itself is not known in this fictional world.
+The A party believes similiar to the novel "Fahrenheit 451" that social media is dangerous and should be banned. 
+The book "Fahrenheit 451" itself is not known in this fictional world.
 The leader of the A party is {NAME_A}.
 The A party is part of the opposition. 
 The other parties are very small. 
@@ -39,7 +39,7 @@ People involved in the discussion:
 
 PROMPT_A = f"""
 You are taking part in an TV discussion in a fictional world. Your name is {NAME_A} and you are a politician and leader of the A party. 
-You really think that books are dangerous and should be banned. 
+You really think that social media is dangerous and should be banned. 
 It is your personal mission.
 If someone uses a quote, you try to use another quote from the same person that prooves your point and not theirs.
 If you answer to a statement of {NAME_B}, pick a weak argument and try to win the viewers over to your side.
@@ -100,35 +100,19 @@ def add_to_history(content):
 
 add_to_history(said(NAME_M, 'Welcome to the discussion! We are talking about the next election. What is your party going to do?'))
 
-
-messageA = get_new_message(clientA,
-                           create_messages(
-                               message("system", SYSTEM_A),
-                               history,
-                               message("user", f"{NAME_A} start the discussion!")
-                           )
-                           )
-print_message(messageA)
-add_to_history(said(NAME_A, messageA))
-
-messageB = get_new_message(clientB, 
-                           create_messages(
-                               message("system", SYSTEM_B),
-                               history,
-                               message("user", f"{NAME_B} what is your answer?")
-                           )
-                           )
-print_message(messageB)
-add_to_history(said(NAME_B, messageB))
-
-messageA = get_new_message(clientA, 
-                           create_messages(
-                                 message("system", SYSTEM_A),
-                                 history,
-                                 message("user", f"{NAME_A} what is your answer?")
+def turn(name, client, system):
+    response = get_new_message(client,
+                            create_messages(
+                                message("system", system),
+                                history,
+                                message("user", f"{name} what is your answer?")
                             )
                             )
-print_message(messageA)
-add_to_history(said(NAME_A, messageA))
+    print_message(response)
+    add_to_history(said(name, response))
+
+turn(NAME_A, clientA, SYSTEM_A)
+turn(NAME_B, clientB, SYSTEM_B)
+turn(NAME_A, clientA, SYSTEM_A)
 
 print(history)
