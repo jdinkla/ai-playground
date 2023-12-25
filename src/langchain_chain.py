6f3bfcbf393b883dd74f2a9656ed1694553cd0d0
Jörn Dinkla <jdinkla@thoughtworks.com>
2023-12-25 14:43:33 +0100

    add langchain chain example

---
+++ src/langchain_chain.py
@@ -0,0 +1,22 @@
+import argparse
+import logging
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from utilities import init
+
+parser = argparse.ArgumentParser()
+parser.add_argument("topic", help="the topic of the joke", default="ice cream", nargs='?')
+args = parser.parse_args()
+
+init(logging.WARNING)
+
+prompt = ChatPromptTemplate.from_template("tell me a short joke about {topic}")
+model = ChatOpenAI()
+output_parser = StrOutputParser()
+
+chain = prompt | model | output_parser
+
+x = chain.invoke({"topic": args.topic})
+
+print(x)
